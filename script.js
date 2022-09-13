ce0047c569fd71e5a377843431bdbec9b68b6df3
Vinicius Duzanski <viniciusduzanski@gmail.com>
2022-09-12 22:06:37 -0300

    Deploy Frontend

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+
+let usuario;
+let senha;
+let users;
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    usuario = e.target.usuario.value;
+    senha = e.target.senha.value;
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(users => checkUser(users));
+})
+
+function redirectPage(url){
+    window.location.href = url;
+}
+
+function checkUser(users){
+    let user = users.find(dbuser => dbuser.usuario === usuario && dbuser.senha === senha);
+    if (user){
+        redirectPage("inicio.html");
+        alert("Logado com sucesso!");
+    } else {
+        alert("Usuário inválido!");
+    }
+}
