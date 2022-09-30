b8f861fdf4cdeb6a7e144bfc2198f9f456462ba1
Vinicius Duzanski <viniciusduzanski@gmail.com>
2022-09-30 19:58:32 -0300

    Remoção do campo Grandeza

--- script-cadastrarsensor.js
+++ script-cadastrarsensor.js
@@ -3,16 +3,14 @@ const form = document.querySelector("form");
 let idDispositivo;
 let idSensor;
 let empresa;
-let grandeza;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     idDispositivo = e.target.idDispositivo.value.trim();
     idSensor = e.target.idSensor.value.trim();
     empresa = e.target.empresa.value.trim();
-    grandeza = e.target.grandeza.value.trim();
 
-    if (idDispositivo === "" || idSensor === "" || empresa === "" || grandeza === "") {
+    if (idDispositivo === "" || idSensor === "" || empresa === "") {
         alert("O campo não pode ser em branco!");
     } else if (isNaN(idDispositivo) || isNaN(idSensor)) {
         alert("ID do Dispositivo e ID do Sensor precisam ser números!")
@@ -20,7 +18,7 @@ form.addEventListener("submit", (e) => {
         fetch('http://localhost:3000/sensores', {
             method: 'post',
             body: JSON.stringify ({
-                idDispositivo, idSensor, empresa, grandeza
+                idDispositivo, idSensor, empresa
             }),
             headers: {
                 "Content-Type": "application/json"
