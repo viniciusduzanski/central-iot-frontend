cbb250643fe4c89ef3bf05c3cbb309074328459c
Vinicius Duzanski <viniciusduzanski@gmail.com>
2022-10-08 12:04:44 -0300

    Renderizar dados na tabela

--- script-solicitardados.js
+++ script-solicitardados.js
@@ -3,6 +3,8 @@ const form = document.querySelector("form");
 let idDispositivo;
 let dtInicial;
 let dtFinal;
+let dados;
+let table = document.querySelector('.js-body');
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,9 +15,27 @@ form.addEventListener("submit", (e) => {
     dtInicial += " 00:00:00";
     dtFinal += " 23:59:59";
 
+    console.log(dtInicial);
+    console.log(dtFinal);
+
     fetch('http://localhost:3000/dados?' + new URLSearchParams({
         idDispositivo, dtInicial, dtFinal
     }))
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => montTable(response))
 });
+
+function montTable(dado) {
+    let row = "";
+    dado.forEach(dado => {
+        row += `
+        <tr>
+            <td>${dado.id_sensor}</td>
+            <td>${dado.data_hora}</td>
+            <td>${dado.valor}</td>
+            <td>${dado.grandeza}</td>
+        </tr>
+        `
+        table.innerHTML = row;
+    })
+};
